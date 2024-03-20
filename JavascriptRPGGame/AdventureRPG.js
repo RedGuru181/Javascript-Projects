c356825308545d0d3727c1bcedab8a85e6f0bd80
RedGuru181 <nbiports@gmail.com>
2024-03-20 18:35:50 -0400

    start of the bag function
    I basically just fiddled around with the bag function trying to get some sort of inventory function. not as easy as I thought it would be.

--- JavascriptRPGGame/AdventureRPG.js
+++ JavascriptRPGGame/AdventureRPG.js
@@ -1,6 +1,39 @@
-let inventory = '';
+
+//Starting health
 let health = 100;
+//Starting gold
 let gold = 50;
+//weapons array for weapons that are in the game
+let weapons = [
+    {
+        name: "Stick",
+        damage: 5,
+        description: "A pretty thick branch that fell off of a tree, good to defend yourself with now I guess.",
+        gold: 0
+    },
+    {
+        name: "Sword",
+        damage: 10,
+        description: "A sharp weapon that deals a bit more damage than a sword.",
+        gold: 20
+    },
+    {
+        name: "Battle Axe",
+        damage: 20,
+        description: "A heavy two handed object, when it strikes an oppenant they probably won't die.",
+        gold: 50
+    }
+];
+
+let potions = [
+    {
+        name: "Health potion",
+        description: "Heals your body and mind."
+
+    }
+]
+//initial inventory
+let inventory = [weapons[0].name, potions[0].name];
 
 //Update status
 function updateStatus() {
@@ -18,9 +51,32 @@ function submitName() {
     const playerName = document.getElementById('playerNameInput').value;
     document.getElementById('playerNameInput').style.display = 'none';
     document.getElementById('descriptionContainer').removeChild(document.getElementById('playerNameInput'));
-    document.getElementById('descriptionContainer').removeChild(document.getElementByTagName('button')[0]);
+    document.getElementById('descriptionContainer').removeChild(document.getElementById('submitName'));
     updateDescription('Welcome, ' + playerName + '! We need your help!')
 }
+
+//Display inventory function
+function displayInventory() {
+    const statusBar = document.getElementById('statusbar');
+    document.getElementById('statusbar').textContent = '';
+    inventory.forEach(item => {
+        const itemElement = document.createElement('p');
+        itemElement.textContent = item;
+        statusBar.appendChild(itemElement);
+    });
+}
+
+
+//Open bag function
+function openBag() {
+    updateDescription("You open your bag and take a peak inside.");
+    displayInventory();
+
+    const bagButton = document.getElementById("bagbtn");
+    bagButton.textContent = "Close bag";
+    bagButton.onclick = closeBag();
+}
+
 //Update initial status
 updateStatus();
 
